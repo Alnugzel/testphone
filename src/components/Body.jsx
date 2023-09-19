9bd722df8d719822cc22e017a9211ca611dcc4a9
Alnugzel <angelfiguera0889@gmail.com>
2023-09-19 16:02:02 -0400

    second commit

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -12,6 +12,15 @@ const Body = () => {
                 Lorem Ipsum Dolor Sit Amet, Consectetur
             </p>
         </header>
+
+    <section className='article__status'>
+        <div><img src="" alt="" /> <p>Cart Review</p></div>
+        <div><img src="" alt="" /> <p>Checkout</p></div>
+        <div><img src="" alt="" /> <p>Special Offer</p></div>
+        <div><img src="" alt="" /> <p>Confirmation</p></div>
+
+    </section>
+
     </article>
   )
 }
